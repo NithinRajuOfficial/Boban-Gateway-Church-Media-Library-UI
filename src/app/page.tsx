91fc140ea113f93f374e060052614ebbb98c94eb
Nithin Raju <nithinraju242@gmail.com>
2025-03-05 09:59:57 +0530

    Sidebar responsive complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,11 @@ import SideBar from "@/components/Sidebar/SideBar";
 
 export default function MediaLibrary() {
   return (
-    <main
-      className="h-screen grid   
-      sm:grid-cols-[374px_1fr]   
-      md:grid-cols-[398px_1fr]  
-      lg:grid-cols-[420px_1fr] 
-      xl:grid-cols-[450px_1fr]
-      grid-rows-[1fr]"
-    >
+    <main className="h-screen flex flex-col sm:flex-row">
+      {/* Sidebar */}
       <SideBar />
+
+      
     </main>
   );
 }
